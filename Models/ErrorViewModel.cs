ae3ddaee2075d9e68e345f5dac65c0ea321db1c3
AmezcuaM02 <santiago_amezcua@hotmail.com>
2025-05-23 09:14:58 -0600

    Inicia proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VideojuegosApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
