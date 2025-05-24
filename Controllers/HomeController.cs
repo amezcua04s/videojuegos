bd5a8426c926575f9d9bf2d779ff36a41c60fe58
AmezcuaM02 <santiago_amezcua@hotmail.com>
2025-05-23 21:08:07 -0600

    Un solo index, navegación entre pantallas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,6 +32,10 @@ namespace VideojuegosApp.Controllers
             return View();
         }
 
+        public IActionResult Login() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
